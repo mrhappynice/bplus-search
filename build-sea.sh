78ca3dec72a333826fdb50d680134bef6997dfd9
Mr. Happy Nice <themrhappynice@gmail.com>
2025-11-07 17:31:47 -0600

    Update build-sea.sh

--- build-sea.sh
+++ build-sea.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 ENTRY="${1:-server.js}"
-APP="${2:-bplus-search-nosearx}"          # no extension; macOS/Linux binary
+APP="${2:-bplus-search}"          # no extension; macOS/Linux binary
 BUNDLE="bundle.cjs"
 BLOB="sea-prep.blob"
 CONFIG="sea-config.json"
